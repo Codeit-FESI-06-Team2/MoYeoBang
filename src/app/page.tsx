ecd5a4a3ac6d1f0434ed7a7808b41036bb3652c5
purplenib <breadjoo1014@naver.com>
2025-03-18 15:15:40 +0900

    FESI1-107 [Refactor]: get 요청 캐싱 및 홈페이지 ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Suspense } from 'react';
 
+import {
+  getGatheringsDateTime,
+  getGatheringsByRegistrationEnd,
+} from '@/axios/gather/apis';
+
 import SkeletonCardList from '@/components/@shared/skeleton/SkeletonCardList';
 import SkeletonSlotList from '@/components/@shared/skeleton/SkeletonSlotList';
 import Carousel from '@/components/home/UI/Carousel';
@@ -8,28 +13,29 @@ import LinkButton from '@/components/@shared/button/LinkButton';
 import NearRecent from '@/components/home/NearRecent';
 import NearDeadlines from '@/components/home/NearDeadlines';
 
-export default function Home() {
+export default async function Home() {
+  const gatheringsDateTime = await getGatheringsDateTime();
+  const gatheringsRegistrationEnd = await getGatheringsByRegistrationEnd();
+
   return (
     <>
-      <div className="mx-auto flex h-full max-w-6xl flex-col gap-12 px-4 pb-16 pt-20 md:gap-20 md:pb-24 md:pt-28 xl:px-0 xl:pb-32 xl:pt-32">
+      <main className="mx-auto flex h-full max-w-6xl flex-col gap-12 px-4 pb-16 pt-20 md:gap-20 md:pb-24 md:pt-28 xl:px-0 xl:pb-32 xl:pt-32">
         <Carousel />
-        <main className="flex flex-col items-center ">
-          <div className="mb-12 flex w-full flex-col items-start gap-2 md:mb-20">
+        <section className="flex flex-col items-center gap-12 md:gap-20">
+          <div className="flex w-full flex-col items-start gap-2">
             <h2 className="text-sm font-medium">함께 할 사람이 없나요?</h2>
             <h1 className="text-2xl font-semibold">지금 여기로 모여방</h1>
           </div>
           <Suspense fallback={<SkeletonCardList />}>
-            <NearRecent />
+            <NearRecent gatherings={gatheringsDateTime} />
           </Suspense>
-          <div className="mt-12 md:mt-20">
-            <LinkButton href="/gathering" variant="dark" size="long">
-              모여방 더보기
-            </LinkButton>
-          </div>
-        </main>
-      </div>
+          <LinkButton href="/gathering" variant="dark" size="long">
+            모여방 더보기
+          </LinkButton>
+        </section>
+      </main>
       <Suspense fallback={<SkeletonSlotList />}>
-        <NearDeadlines />
+        <NearDeadlines gatherings={gatheringsRegistrationEnd} />
       </Suspense>
       <Links />
     </>
